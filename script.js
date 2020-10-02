6a97d733a036ec2b64d3b64dc16531b8bdebfff3
BrandonVA <codebva@gmail.com>
2020-10-02 09:09:40 -0700

    Created localStorage obj and updated dom with current values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    console.log('hello world');
+    // console.log('hello world');
 
     //let time = moment().format('LT');
      let todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -37,33 +37,51 @@ $(document).ready(function(){
         ///////////////////////////////////////////checking logic
         // morningOrEvening = 'PM';
         // timeToConvert = 2;
-        console.log(timeToConvert);
+        // console.log(timeToConvert);
 
 
         // Adding checks for if its am or pm 
         if (morningOrEvening === 'PM') {
             // if the time was pm and its the time is not 12(pm)
             timeToConvert !== 12 ? timeToConvert = (timeToConvert * 100) + 1200 : timeToConvert *= 100;
-
-            
         } else {
             // if the time is 12(am)
             timeToConvert === 12 ? timeToConvert = 0: timeToConvert *= 100;
-            
         }
-        
         return timeObj.militaryTime = timeToConvert;
     }
 
     convertToMilitaryTime()
 
 
+
+
+
+
+
+
+
+
     $(".row").each(function() {
         let checkDataHour = $(this).attr('data-hour');
-        checkDataHour = parseInt(checkDataHour);
+
+        
+        let textAreaDivEl = $(this).children('div.textDiv');
+        let textareaDataName = textAreaDivEl.children('textarea').attr('data-name');
+        console.log(textareaDataName);
+
+
+
+
+
+
+        // checkDataHour = parseInt(checkDataHour);
+        checkDataHour = parseInt(checkDataHour)
         console.log(checkDataHour);
         console.log('----------');
         console.log(timeObj.militaryTime);
+
+
         if ( checkDataHour < timeObj.militaryTime ) {
             $(this).children('div .col-8').addClass('past');
         }
@@ -73,8 +91,20 @@ $(document).ready(function(){
         if ( checkDataHour > timeObj.militaryTime ) {
             $(this).children('div.col-8').addClass('future');
         }
+
+        if(localStorage.getItem('hourlyLog') !== null) {
+            let hourlyLogObj = JSON.parse(localStorage.getItem('hourlyLog'));
+            if ( hourlyLogObj.hasOwnProperty(textareaDataName) ) {
+                textAreaDivEl.children('textarea').val(hourlyLogObj[textareaDataName])
+                console.log( hourlyLogObj[textareaDataName] );
+
+            }
+        }
+
+
+
     })
-    console.log(timeObj.militaryTime);
+    // console.log(timeObj.militaryTime);
 
 
    
@@ -93,33 +123,62 @@ $(document).ready(function(){
     currentDayEl.text(timeObj.currentDay);
 
     $('.saveBtn').on('click', function(){
+
+
+
+        
         let textAreaEl = $(this).prev().children('textarea');
         let textAreaElValue = textAreaEl.val();
-        console.log(textAreaElValue);
-        thisLocalStorageEl = textAreaEl.attr('data-name');
-        console.log(thisLocalStorageEl);
+        let textareaDataName = textAreaEl.attr('data-name');
+        console.log(textareaDataName);
 
-
-
-        if (localStorage.getItem(thisLocalStorageEl) === null) {
-            if (textAreaElValue === undefined) {
-                textAreaElValue = 'Hello World'
-            }
-            localStorage.setItem('thisLocalStorageEl',textAreaElValue);
-        let newElement = localStorage.getItem(thisLocalStorageEl) ;
-        console.log(newElement);
-        newElement = 'Hello world'
-        textAreaEl.value = localStorage.getItem(textAreaEl)
-        }
 
 
         // first add a check to see if value( textarea.val() ) was filled out if not than ask to fill it out 
+        if ( textAreaElValue === '') {
+                alert('Please fill in the field.')
+            } else {
+
+            // Check if localStorage obj exists if not need to creat an object for local storage 
+            if (localStorage.getItem('hourlyLog') === null) { 
+                localStorage.setItem('hourlyLog','{}'); 
+            }
+            // Once object is created parse it and set to new var to so we can manipulate and add key value pairs
+            let old_hourlyLog = JSON.parse(localStorage.getItem('hourlyLog'));
+
+            // once created when clicked see if obj hasOwnProperty of textarea data-name attr 
+            if (old_hourlyLog.hasOwnProperty(textareaDataName) === false) {
+                // if ture add it
+                old_hourlyLog[textareaDataName] = textAreaElValue;
+                alert('porp created')
+            } else {
+                // if not than update property value   ------------------------Might be able to omit the first check? test later
+                old_hourlyLog[textareaDataName] = textAreaElValue;
+            }
 
 
-        // need to creat an object for local storage 
-        // once created when clicked see if obj hasOwnProperty of textarea data-name attr 
-        // if fasle add it
-        // if not than update property value
+            console.log(old_hourlyLog);
+            // console.log(old_hourlyLog[textareaDataName]);
+
+            let new_houlyLog = JSON.stringify(old_hourlyLog);
+            localStorage.setItem('hourlyLog',new_houlyLog);
+    
+            console.log(localStorage.getItem('hourlyLog'));
+        }
+
+
+
+
+
+
+
+  
+
+
+        
+
+
+
 
 
 
